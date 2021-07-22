1a6be313378878f37111d31e0656a28065ad350a
thengo1 <tbn2109@columbia.edu>
2021-07-22 12:18:49 -0400

    adding initial intro page, todo: add in typing effect and animations

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,10 @@ import "./intro.scss"
 export default function Intro() {
     return( 
         <div className="intro" id="intro">
-            
+            <h1>hey! my name is thế ngô.</h1>
+            <p>i am a... <span>fullstack software engineer</span> 🧑‍💻</p>
+            <p>recent grad @ <span>columbia</span> 🦁</p>
+            <p>former swe intern @ <span>nasa jpl</span> 🚀</p>
         </div>
     )
 }
