c6f544c27e95681565391919d7fc9fca2033552b
thengo1 <tbn2109@columbia.edu>
2021-07-22 11:59:27 -0400

    finished navbar and menu options

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/navbar/Navbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
-import "./app.scss"
+import Menu from "./components/menu/Menu";
+import "./app.scss";
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Navbar />
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
 import "./navbar.scss"
 
-export default function Navbar() {
+export default function Navbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="navbar">
-            
+        <div className={"navbar " + (menuOpen && "active")}>
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">tn</a>
+                </div>
+                <div className="right">
+                    <div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
